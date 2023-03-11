6d869c2f7ce5248495e55a86e28e30908fe085af
vklsaravanan <ssaranartist@gmail.com>
2023-03-11 21:09:14 +0530

    new commit

---
+++ check.js
@@ -0,0 +1,81 @@
+var a=document.getElementById(
+"journey-div"
+)
+var b=0;
+var journeys=[
+    {
+        year:"2002",
+        month:"Feb",
+        image:"https://img.icons8.com/ios-filled/1x/birth-date.png",
+        title:"Born cute Little boy",
+        desc:""
+    },
+    {
+        year:"2017",
+        month:"Feb",
+        image:"https://img.icons8.com/glyph-neue/256/school-building.png",
+        title:"Complete 11th standard studies",
+        desc:"Arignar anna government Hr.sec School"
+    },
+    {
+        year:"2019",
+        month:"Feb",
+        image:"https://img.icons8.com/glyph-neue/256/school-building.png",
+        title:"Complete 12th standard studies",
+        desc:"Arignar anna government Hr.sec School"
+    },
+    {
+        year:"2019",
+        month:"Feb",
+        image:"https://img.icons8.com/ios-filled/256/graduation-cap.png",
+        title:"join professional course (ECE)",
+        desc:"VSB Engineering Collage, Karur"
+    },
+    {
+        year:"2022",
+        month:"Nov",
+        image:"https://img.icons8.com/ios-filled/256/manager.png",
+        title:"join as a Project Trainee",
+        desc:"ZOHO Corporation chennai"
+    },
+    {
+        year:"2023",
+        month:"Feb",
+        image:"https://img.icons8.com/ios-filled/256/manager.png",
+        title:"Say bye to Project Trainee",
+        desc:"ZOHO Corporation chennai"
+    },
+    {
+        year:"2023",
+        month:"Mar",
+        image:"https://img.icons8.com/ios-filled/256/manager.png",
+        title:"Start my own project",
+        desc:"Application"
+    },
+]
+
+function journeyAdder(){
+    if(journeys.length <= b){
+        clearInterval(intervalJourney);
+        return
+    }
+    var [year,month,image,title,desc]=Object.values(this.journeys[b++])
+    let journey=journeyCreater(year,month,image,title,desc)
+    function journeyCreater(year,month,image,title,desc){
+
+    return `<div id="journey-card" class="moveTransition">
+        <span>
+            <p>`+year+`</p>
+            <p class="description">`+month+`</p>
+        </span>
+        <span><img src=`+image+` alt="⊛"></span>
+        <span>
+            <p>`+title+`</p>
+            <p class="description">`+desc+`</p>
+        </span>
+        </div>`
+    }
+    this.a.insertAdjacentHTML("afterend",journey);
+}
+let intervalJourney=setInterval(journeyAdder,1000);
+
