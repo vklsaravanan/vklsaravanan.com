f1b079f364076fb03190313f908e0dd3b42abdfd
vklsaravanan <ssaranartist@gmail.com>
2023-05-06 16:05:33 +0530

    update menu button

--- scripts/action.js
+++ scripts/action.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#menu-btn').click(function() {
-      $('#menuList').toggleClass('menu-btn-toggle');
+      $('#menuList').toggleClass('menu-toggle');
+      $('#menu-btn').toggleClass("menu-btn-toggle");
     });
   });
