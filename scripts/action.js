f3841363d1501e10e96e12accb8ac2ec135b08ee
saravanan <saravananraja9095@gmail.com>
2024-03-13 12:45:39 +0530

    typing description animation added

--- scripts/action.js
+++ scripts/action.js
@@ -45,3 +45,33 @@ $(document).ready(function() {
       autoInsertCss: true,
   
     });
+
+//  typing description
+
+function animateText(textArea) {
+let text = textArea.innerHTML;
+let to = text.length,
+from = 0;
+
+animate({
+duration: 5000,
+timing: bounce,
+draw: function(progress) {
+let result = (to - from) * progress + from;
+textArea.innerHTML = text.slice(0, Math.ceil(result))
+}
+});
+}
+
+
+function bounce(timeFraction) {
+for (let a = 0, b = 1; 1; a += b, b /= 2) {
+if (timeFraction >= (7 - 4 * a) / 11) {
+return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+}
+}
+}
+
+animateText($(`#self-intro`)[0]); 
+animateText($(`#career-objective`)[0]);
+
